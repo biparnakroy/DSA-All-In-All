94c0a799854fda1ad4db4a891e57d8ba264fbb38
Biparnak Roy <roybiparnak@gmail.com>
2021-10-05 15:56:36 +0530

    i have no life

---
+++ love_babbar_450/arrays/minumum_swaps.cpp
@@ -0,0 +1,17 @@
+/*
+https://practice.geeksforgeeks.org/problems/minimum-swaps-required-to-bring-all-elements-less-than-or-equal-to-k-together4847/1
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++) cin>>a[i];
+    
+
+    return 0;
+}
