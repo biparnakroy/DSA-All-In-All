ad8ff053a126d9718b163d104b2c62c135e4021f
Biparnak Roy <roybiparnak@gmail.com>
2024-08-29 18:56:04 +0530

    Time: 47 ms (97.63%) | Memory: 27.5 MB (72.35%) - LeetSync

---
+++ leetcode/15-3sum/3sum.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> result;
+        sort(nums.begin(), nums.end()); // Sort the array
+
+        for (int i = 0; i < nums.size(); i++) {
+            // Skip duplicates
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+
+            int left = i + 1;
+            int right = nums.size() - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+                    // Found a triplet
+                    result.push_back({nums[i], nums[left], nums[right]});
+
+                    // Skip duplicates for left and right pointers
+                    while (left < right && nums[left] == nums[left + 1])
+                        left++;
+                    while (left < right && nums[right] == nums[right - 1])
+                        right--;
+
+                    // Move pointers
+                    left++;
+                    right--;
+                } else if (sum < 0) {
+                    left++; // We need a larger sum
+                } else {
+                    right--; // We need a smaller sum
+                }
+            }
+        }
+
+        return result;
+    }
+};
