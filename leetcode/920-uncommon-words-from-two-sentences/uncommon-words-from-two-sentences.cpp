f003dfe683bd44089929ab2c3bc39fae17dfa488
Biparnak Roy <roybiparnak@gmail.com>
2024-09-17 05:44:25 +0530

    Time: 5 ms (15.91%) | Memory: 8.9 MB (45.08%) - LeetSync

--- leetcode/920-uncommon-words-from-two-sentences/uncommon-words-from-two-sentences.cpp
+++ leetcode/920-uncommon-words-from-two-sentences/uncommon-words-from-two-sentences.cpp
@@ -4,14 +4,13 @@ public:
         unordered_map<string, int> wordCount;
         vector<string> result;
         
-        stringstream ss(s1 + " " + s2); // Combine both sentences with a space between
+        stringstream ss(s1 + " " + s2); 
         string word;
         
         while (ss >> word) {
             wordCount[word]++;
         }
         
-        // Find all the words that appear exactly once
         for (auto& [word, count] : wordCount) {
             if (count == 1) {
                 result.push_back(word);
