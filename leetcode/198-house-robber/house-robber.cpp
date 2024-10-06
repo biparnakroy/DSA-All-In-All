0de4f0b9e43d1fc69c314252e66d780b731c2949
Biparnak Roy <roybiparnak@gmail.com>
2024-10-06 13:07:37 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (87.40%) - LeetSync

---
+++ leetcode/198-house-robber/house-robber.cpp
@@ -0,0 +1,19 @@
+class Solution {
+    int dp[101];
+private:
+    int help(vector<int>& nums, int idx){
+        if(idx == 0) return nums[0];
+        if(idx < 0) return 0;
+        if(dp[idx]!=-1) return dp[idx];
+        int pick = nums[idx]  + help(nums,idx-2);
+        int not_pick = 0  + help(nums, idx-1);
+        return dp[idx]=max(pick, not_pick);
+    }
+public:
+    int rob(vector<int>& nums) {
+        int res = 0;
+        memset(dp, -1, sizeof(dp));
+        res = help(nums, nums.size()-1);
+        return res;
+    }
+};
