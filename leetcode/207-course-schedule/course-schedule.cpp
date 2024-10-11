77184abf88e9d622239d8ed6b1bfd70aa9838535
Biparnak Roy <roybiparnak@gmail.com>
2024-10-12 02:34:50 +0530

    Time: 20 ms (39.17%) | Memory: 18.4 MB (51.36%) - LeetSync

---
+++ leetcode/207-course-schedule/course-schedule.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> graph(numCourses);
+        vector<int> indegree(numCourses, 0);
+
+        // Build graph and compute indegree of each node
+        for (auto& prereq : prerequisites) {
+            int u = prereq[1]; // Course to be completed first
+            int v = prereq[0]; // Course depending on the previous course
+            graph[u].push_back(v);
+            indegree[v]++;
+        }
+
+        // Initialize queue with all courses having no prerequisites
+        queue<int> q;
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                q.push(i);
+            }
+        }
+
+        vector<int> order;
+
+        // Kahn's algorithm for topological sorting (BFS)
+        while (!q.empty()) {
+            int course = q.front();
+            q.pop();
+            order.push_back(course);
+
+            for (int neighbor : graph[course]) {
+                indegree[neighbor]--;
+                if (indegree[neighbor] == 0) {
+                    q.push(neighbor);
+                }
+            }
+        }
+
+        // If the number of courses in the order is less than numCourses, a
+        // cycle exists
+        if (order.size() == numCourses) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+};
